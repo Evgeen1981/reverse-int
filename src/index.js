efc4955cd8180fa4dd0b21cbeeaf91de31b3265c
Evgeen <evgeen@inbox.ru>
2021-02-08 20:31:17 +0300

    Complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let numN = 0;
+    for (; n; n = Math.floor(n / 10)) {
+        numN *= 10;
+        numN += n % 10;
+    }
+    n = numN;
+    return n;
+};
